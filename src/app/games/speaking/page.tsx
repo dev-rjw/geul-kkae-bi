36976181004a6a52e6e559f1434aa81f4b3cc45b
lim <dntksql1010@naver.com>
2024-11-04 12:55:59 +0900

    update:[speak] 타이머, css, 오디오 기능 수정

--- src/app/games/speaking/page.tsx
+++ src/app/games/speaking/page.tsx
@@ -1,11 +1,13 @@
+import { Metadata } from 'next';
 import Speak from './_components/Speak';
 
+export const metadata: Metadata = {
+  title: '주어진 문장 읽기 게임',
+  description: '주어진 문장 읽고 정확도 확인하는 게임',
+};
+
 const page = () => {
-  return (
-    <div className='h-90vh bg-[#FCFBF9]'>
-      <Speak />
-    </div>
-  );
+  return <Speak />;
 };
 
 export default page;
