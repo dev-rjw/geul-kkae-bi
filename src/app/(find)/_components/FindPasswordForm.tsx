3258fee53ca16f9480845570f07708aa8282b885
ayoung <jo.ayoung.1210@gmail.com>
2024-11-07 05:40:17 +0900

    update: 소셜로그인 회원 비밀번호 찾기 막기

--- src/app/(find)/_components/FindPasswordForm.tsx
+++ src/app/(find)/_components/FindPasswordForm.tsx
@@ -8,6 +8,8 @@ import { findPasswordSchema } from '@/schemas/findSchema';
 import { findPassword } from '@/utils/auth/client-action';
 import DefaultButton from '@/components/DefaultButton';
 import EmailInput from '@/components/EmailInput';
+import { fetchEmailUserInfo } from '@/utils/user/client-action';
+import Swal from 'sweetalert2';
 
 const FindPasswordForm = () => {
   // 유효성 검사
@@ -23,7 +25,24 @@ const FindPasswordForm = () => {
 
   const onSubmit = async (values: FieldValues) => {
     const { email } = values;
-    findPassword(email);
+
+    const user = await fetchEmailUserInfo(email);
+
+    console.log(user);
+
+    if (user?.provider === 'google' || user?.provider === 'kakao') {
+      Swal.fire({
+        html: `<div class="text-gray-700">해당 계정은 ${user?.provider} 소셜 로그인으로 가입되었습니다. <br />${user?.provider} 계정으로 로그인해주세요.</div>`,
+        customClass: {
+          title: 'swal-custom-title',
+          htmlContainer: 'swal-custom-text',
+          confirmButton: 'swal-custom-button',
+        },
+        confirmButtonText: '로그인하기',
+      });
+    } else {
+      findPassword(email);
+    }
   };
 
   return (
