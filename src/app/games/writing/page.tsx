3a14c098cf2350f8fc32644304d46819d20b682f
hanamstarfield <qnfdmlto@naver.com>
2024-11-06 14:25:50 +0900

    feat: 타임 초과 시 다음 퀴즈 비활성화

--- src/app/games/writing/page.tsx
+++ src/app/games/writing/page.tsx
@@ -64,12 +64,13 @@ const WritingQuizPage = () => {
   // 다음 문제로 넘어가기, 퀴즈 클리어
   const moveToNextQuiz = (e: React.FormEvent) => {
     e.preventDefault();
+    if (isTimeOver) return;
+
     if (currentQuizIndex < questions.length - 1) {
       setCurrentQuizIndex((index) => index + 1);
       handleCheckAnswer();
       setUserInput('');
     } else {
-      console.log('최종 점수는?', score);
       saveScore();
       setIsAllQuestions(true);
     }
@@ -103,7 +104,6 @@ const WritingQuizPage = () => {
         .select('id, writing')
         .eq('user_id', userId)
         .eq('week', weekNumber);
-      console.log('뭐라고 나옴?', currentScore);
       if (fetchError) {
         console.error('기존 랭크 데이터를 가져오는 중 오류가 발생했습니다.', fetchError);
         return;
@@ -191,31 +191,27 @@ const WritingQuizPage = () => {
       </div>
 
       <div className=' absolute top-1/2 right-[1.25rem] transform -translate-y-1/2 flex flex-col items-center'>
-        {!isAllQuestions && !isTimeOver && (
-          <div className='flex flex-col items-center'>
-            <p className='text-center text-2xl font-medium mb-2'>{`${currentQuizIndex + 1}/10`}</p>
-            <button
-              onClick={moveToNextQuiz}
-              className='px-4 py-2'
-            >
-              <Image
-                src='/icon_btn_writing.svg'
-                alt='nextbutton'
-                width={48}
-                height={48}
-                style={{ width: 'auto', height: 'auto' }}
-              />
-            </button>
-          </div>
-        )}
-        {(isAllQuestions || isTimeOver) && (
+        <div className='flex flex-col items-center'>
+          <p className='text-center text-2xl font-medium mb-2'>{`${currentQuizIndex + 1}/10`}</p>
           <button
-            onClick={moveToWritingResultPage}
-            className='text-2xl font-medium'
+            onClick={moveToNextQuiz}
+            className='px-4 py-2'
           >
-            결과 보기
+            <Image
+              src='/icon_btn_writing.svg'
+              alt='nextbutton'
+              width={48}
+              height={48}
+              style={{ width: 'auto', height: 'auto' }}
+            />
           </button>
-        )}
+        </div>
+        <button
+          onClick={moveToWritingResultPage}
+          className={`text-2xl font-medium ${isTimeOver || isAllQuestions ? 'visible' : 'invisible'}`}
+        >
+          결과 보기
+        </button>
       </div>
     </div>
   );
