5e5e69074110bc56680a82d3284b50d18f27fc1a
lim <dntksql1010@naver.com>
2024-11-11 17:26:59 +0900

    refactor:[speak] 오디오 영역 리팩토링

--- src/app/games/speaking/_components/Speak.tsx
+++ src/app/games/speaking/_components/Speak.tsx
@@ -10,6 +10,10 @@ import icon from '../../../../../public/ico_audio.png';
 import { timeStore } from '@/store/timeStore';
 import Tutorial from './Tutorial';
 
+function getRandomQuestion(textArray: string[]) {
+  return textArray.sort(() => Math.random() - 0.5).slice(0, 10);
+}
+
 const Speak = () => {
   const [randomText, setRandomText] = useState<string[]>([]);
   const mediaRecorderRef = useRef<MediaRecorder | null>(null);
@@ -17,43 +21,35 @@ const Speak = () => {
   const { text, isRecording, setText, setIsRecording, setIsLoading, resetText, resetPercent, resetIndex } =
     speekStore();
   const { isDelay, resetTimer, setIsDelay } = timeStore();
-  function getRandomSentences(textArray: string[]) {
-    return textArray.sort(() => Math.random() - 0.5).slice(0, 10);
-  }
+
+  const ondataavailable = (event: { data: Blob }) => {
+    audioChunks.current = [event.data];
+  };
+
+  const onStop = async () => {
+    const audioBlob = new Blob(audioChunks.current, { type: 'audio/wav' });
+    audioChunks.current = [];
+    const pcmData = await convertAudioToPCM(audioBlob);
+    const data = await sendToAudio(pcmData);
+    if (data) {
+      const jsonText = await data.trim().split(/\n(?={)/);
+      const jsonArray = jsonText.map((part) => JSON.parse(part.trim()));
+      const text = jsonArray[jsonArray.length - 1];
+      await setText(text.text);
+    }
+  };
 
   useEffect(() => {
-    const questions = getRandomSentences(TextData);
+    const questions = getRandomQuestion(TextData);
     setRandomText(questions);
     const initMediaRecorder = async () => {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
         const recorder = new MediaRecorder(stream, { audioBitsPerSecond: 8000 });
         mediaRecorderRef.current = recorder;
-        recorder.ondataavailable = (event) => {
-          audioChunks.current = [event.data];
-        };
-        recorder.onstop = async () => {
-          const audioBlob = new Blob(audioChunks.current, { type: 'audio/wav' });
-          audioChunks.current = [];
-          // 음성확인
-          // const audioUrl = URL.createObjectURL(audioBlob);
-          // const audioElement = new Audio(audioUrl);
-          // audioElement.play();
-
-          // const data = await sendToAudio(audioBlob);
-          // const data = await sendToAudio(await convertAudioToPCM(audioBlob));
-          const pcmData = await convertAudioToPCM(audioBlob);
-          const data = await sendToAudio(pcmData);
-          if (data) {
-            const jsonText = await data.trim().split(/\n(?={)/);
-            const jsonArray = jsonText.map((part) => JSON.parse(part.trim()));
-            const text = jsonArray[jsonArray.length - 1];
-            await setText(text.text);
-          }
-          setIsLoading(false);
-          // 포맷확인
-          // console.log(audioBlob);
-        };
+        recorder.ondataavailable = ondataavailable;
+        recorder.onstop = onStop;
+        setIsLoading(false);
       } catch (error) {
         alert('마이크 권한이 필요합니다');
         console.error(error);
@@ -61,11 +57,11 @@ const Speak = () => {
     };
     initMediaRecorder();
     return () => {
-      setRandomText([]); // 랜덤 문장 초기화
-      resetText(); // 텍스트 초기화
+      setRandomText([]);
+      setIsDelay(false);
+      resetText();
       resetPercent();
       resetTimer();
-      setIsDelay(false);
       resetIndex();
     };
   }, []);
