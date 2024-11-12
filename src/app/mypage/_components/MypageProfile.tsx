ec3363ad94c3cbeca1e3775bf88fe00c78d7a710
ayoung <jo.ayoung.1210@gmail.com>
2024-11-12 13:03:27 +0900

    fix: 중복 코드 삭제, 회원탈퇴 작업 중

--- src/app/mypage/_components/MypageProfile.tsx
+++ src/app/mypage/_components/MypageProfile.tsx
@@ -6,7 +6,7 @@ import Avatar from '@/components/Avatar';
 import LineTitle from '@/components/LineTitle';
 import { useAuth } from '@/queries/useAuth';
 import { User } from '@/types/mypage';
-import { fetchCurrentUserInfo } from '@/utils/user/client-action';
+import { fetchCurrentUserInfoByEmail } from '@/utils/user/client-action';
 import DefaultButton from '@/components/DefaultButton';
 import Link from 'next/link';
 import { createClient } from '@/utils/supabase/client';
@@ -21,7 +21,7 @@ const MypageProfile = () => {
   useEffect(() => {
     if (data) {
       const email = data?.user_metadata.email;
-      fetchCurrentUserInfo(email).then((elemant) => setUser(elemant));
+      fetchCurrentUserInfoByEmail(email).then((elemant) => setUser(elemant));
     }
   }, [data]);
 
@@ -45,6 +45,58 @@ const MypageProfile = () => {
 
       router.push('/');
     }
+  };
+
+  const deleteUser = async () => {
+    try {
+      console.log('try>>>>>>> ');
+      await supabase.functions.invoke('user-self-deletion');
+
+      Swal.fire({
+        html: `<div class="text-gray-700">회원탈퇴가 완료되었습니다.</div>`,
+        customClass: {
+          title: 'swal-custom-title',
+          htmlContainer: 'swal-custom-text',
+          confirmButton: 'swal-custom-button',
+        },
+        confirmButtonText: '확인',
+      });
+    } catch (error) {
+      Swal.fire({
+        html: `<div class="text-gray-700">회원탈퇴에 실패했습니다. 다시 시도해 주세요.</div>`,
+        customClass: {
+          title: 'swal-custom-title',
+          htmlContainer: 'swal-custom-text',
+          confirmButton: 'swal-custom-button',
+        },
+        confirmButtonText: '확인',
+      });
+      console.error('회원탈퇴 중 오류 발생:', error);
+    } finally {
+      console.log('finally>>>>>>> 성공');
+
+      // await supabase.auth.signOut();
+      // router.push('/');
+    }
+  };
+
+  const handleDeleteUser = async () => {
+    Swal.fire({
+      html: `<div class="text-gray-700">정말 탈퇴하시겠습니까?</div><div class="text-lg text-gray-500 mt-2">탈퇴 버튼 선택 시, <br />계정은 삭제되며 복구되지 않습니다.</div>`,
+      customClass: {
+        title: 'swal-custom-title',
+        htmlContainer: 'swal-custom-text',
+        confirmButton: 'swal-custom-button',
+        cancelButton: 'swal-custom-button',
+      },
+      confirmButtonText: '탈퇴',
+      showCancelButton: true,
+      cancelButtonText: '취소',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteUser();
+      }
+    });
   };
 
   return (
@@ -87,7 +139,15 @@ const MypageProfile = () => {
             <Link href='/mypage/change-password'>비밀번호 변경</Link>
           </DefaultButton>
         </div>
-        <div className='mt-auto text-center'>
+        <div className='flex items-center justify-center gap-4 mt-auto text-center'>
+          <DefaultButton
+            variant='text'
+            className='text-white'
+            onClick={handleDeleteUser}
+          >
+            회원탈퇴
+          </DefaultButton>
+
           <DefaultButton
             variant='text'
             className='text-white'
