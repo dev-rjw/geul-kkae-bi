bbb6f927f434c7f15225b4ec9f2a328fa8fb8844
boyoung-12 <boyoung182@gmail.com>
2024-11-13 23:23:40 +0900

    fix:[result]nickname 변경되지 않는 문제 해결

--- src/utils/auth/server-action.ts
+++ src/utils/auth/server-action.ts
@@ -28,15 +28,3 @@ export const fetchUserId = async () => {
     return null;
   }
 };
-
-// 로그인한 유저 닉네임 가져오기
-export const fetchUserNickName = async () => {
-  const supabase = createClient();
-  const { data: user } = await supabase.auth.getUser();
-
-  if (user) {
-    return user.user?.user_metadata.nickname;
-  } else {
-    return null;
-  }
-};
