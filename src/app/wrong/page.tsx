60219ff2d8dbd54ef5e250be784b9c16432377ba
lim <dntksql1010@naver.com>
2024-11-15 19:53:35 +0900

    feat:[speak] 오답페이지 구현

--- src/app/wrong/page.tsx
+++ src/app/wrong/page.tsx
@@ -1,23 +1,11 @@
-'use client';
-
-import { useAuth } from '@/queries/useAuth';
-import { useGetWrongAnswer } from '@/queries/useGetWrongAnswer';
-import { weekNumber } from '@/utils/week/weekNumber';
-import React from 'react';
+import SpeakWrong from './_components/SpeakWrong';
 
 const AnswerPage = () => {
-  const { data: user } = useAuth();
-  const { data: answer } = useGetWrongAnswer(user?.id, 'speaking', weekNumber);
-
-  const textArray = answer?.map((item) => JSON.parse(item.answer));
-  const wrongAnswerArray = Array.from(
-    new Map(textArray?.flat().map((item) => [item.text, { text: item.text, score: item.score }])).values(),
+  return (
+    <div>
+      <SpeakWrong />
+    </div>
   );
-
-  console.log(answer);
-  console.log(textArray);
-  console.log(wrongAnswerArray);
-  return <div></div>;
 };
 
 export default AnswerPage;
--- src/queries/useGetWrongAnswer.ts
+++ src/queries/useGetWrongAnswer.ts
@@ -1,25 +1,25 @@
 import browserClient from '@/utils/supabase/client';
 import { useQuery } from '@tanstack/react-query';
 
-const fetchWrongAnswer = async (user_id: string | undefined, gameType: string, weekNumber: number) => {
+const fetchWrongAnswer = async (user_id: string | null | undefined, weekNumber: number) => {
   const { data, error } = await browserClient
     .from('answer')
     .select('*')
     .eq('user_id', user_id)
     .eq('week', weekNumber)
-    .eq('game', gameType);
+    .eq('game', 'speaking');
 
   if (error || !user_id) {
     throw new Error();
-    console.log('error');
   } else {
     return data;
   }
 };
 
-export const useGetWrongAnswer = (user_id: string | undefined, gameType: string, weekNumber: number) => {
+export const useGetWrongAnswer = (user_id: string | null | undefined, weekNumber: number) => {
   return useQuery({
     queryKey: ['answer'],
-    queryFn: () => fetchWrongAnswer(user_id, gameType, weekNumber),
+    queryFn: () => fetchWrongAnswer(user_id, weekNumber),
+    enabled: Boolean(user_id),
   });
 };
