194bf5be098b5aab6d9a4573880fa1944366f4b3
boyoung-12 <boyoung182@gmail.com>
2024-11-19 04:03:29 +0900

    feat:[rank]등수 퍼센트 로직 구현, shadcn 동적 그래프 적용 add: PercentGraph.tsx 생성

--- src/app/games/rank/page.tsx
+++ src/app/games/rank/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Rank } from '@/types/rank';
 import { fetchUserId } from '@/utils/auth/server-action';
 import Image from 'next/image';
 import './style.css';
@@ -12,8 +11,8 @@ import {
   insertLastRankingData,
 } from '@/utils/rank/server-action';
 import { redirect } from 'next/navigation';
-import LineTitle from '@/components/LineTitle';
 import { fetchUserProfile } from '@/utils/user/server-action';
+import PercentGraph from './_components/PercentGraph';
 
 const RankingPage = async () => {
   const userId = await fetchUserId();
@@ -21,8 +20,9 @@ const RankingPage = async () => {
   const userProfile = await fetchUserProfile(userId);
 
   //이번주 랭킹 로직
-  let userTable;
-  let countRanking;
+  let myRankingThisWeek;
+  let countRankingThisWeek;
+  let myRankPercentThisWeek;
 
   if (latestWeekData) {
     const latestWeek = latestWeekData.week;
@@ -32,198 +32,194 @@ const RankingPage = async () => {
     if (!data || data.length === 0) {
       redirect('/');
     } else {
-      //이번주 전체 등수
-      countRanking = data.map((item, index) => ({ ...item, ranking: index + 1 }));
+      //이번주 전체 등수 매기기
+      countRankingThisWeek = data.map((item, index) => ({ ...item, ranking: index + 1 }));
       //이번주 내 등수
-      userTable = countRanking?.filter((user) => user.user_id === userId);
+      myRankingThisWeek = countRankingThisWeek?.filter((user) => user.user_id === userId);
+
+      //내 등수 퍼센트 계산
+      if (myRankingThisWeek && myRankingThisWeek.length > 0) {
+        const myRank = myRankingThisWeek[0]?.ranking;
+        const totalUsers = countRankingThisWeek?.length;
+
+        myRankPercentThisWeek = ((1 - (myRank - 1) / (totalUsers - 1)) * 100).toFixed(2);
+      }
     }
   }
+  console.log('myRankPercentThisWeek', myRankPercentThisWeek);
 
   //지난주 랭킹 로직
+  let myRankingLastWeek;
+  let lastWeekData;
+  let myRankPercentLastWeek;
+
   if (latestWeekData && latestWeekData.week - 1 > 0) {
     const lastWeek = latestWeekData.week - 1;
 
-    const lastWeekData = await fetchLastWeek(lastWeek);
+    lastWeekData = await fetchLastWeek(lastWeek);
 
     if (lastWeekData?.[0].ranking === null) {
-      const countRanking: Rank[] | undefined = lastWeekData?.map((item, index) => ({
+      const countLastWeekRanking = lastWeekData?.map((item, index) => ({
         ...item,
         ranking: index + 1,
       }));
 
-      if (countRanking) {
-        insertLastRankingData(countRanking);
+      if (countLastWeekRanking) {
+        insertLastRankingData(countLastWeekRanking);
       }
     }
+    const myRank = await fetchUserLastRank(userId, lastWeek);
 
-    const myLastRank = await fetchUserLastRank(userId, lastWeek);
+    if (myRank && myRank.ranking !== null && lastWeekData) {
+      myRankingLastWeek = myRank?.ranking;
 
-    return (
-      <div className='container pt-10 pb-4 max-md:px-0 max-md:pt-[14rem]'>
-        <div
-          className='scrollbar-primary flex flex-col pt-8 pb-[9.375rem] rounded-[3.125rem] bg-primary-50 '
-          style={{ height: 'calc(100vh - 136px)' }}
-        >
-          <div className='max-md:hidden flex items-center justify-center gap-x-2'>
-            <Image
-              src='/icon_rank.svg'
-              width={45}
-              height={45}
-              quality={85}
-              alt='랭킹순위 옆 아이콘'
-            />
-            <h2 className='title-36 text-primary-400 mt-1 -mb-2'>이번주 전체 랭킹 순위</h2>
-          </div>
-          <div className='hidden max-md:inline-flex'>
-            <LineTitle
-              className='text-primary-400'
-              lineClassName='bg-primary-100'
-            >
-              <Image
-                src='/icon_rank.svg'
-                width={45}
-                height={45}
-                quality={85}
-                alt='랭킹순위 옆 아이콘'
-              />
-              이번주 전체 랭킹 순위
-            </LineTitle>
-          </div>
-          <div className='h-full mx-4 mt-7 overflow-y-scroll'>
-            <div className='grid gap-5 pl-[4.75rem] pr-[3.813rem] pb-5'>
-              {countRanking?.slice(0, 3)?.map((item) => (
-                <div
-                  key={item.id}
-                  className='top-rank flex items-center bg-[#98A7F1] w-full h-[6.25rem] px-8 py-2 mx-auto rounded-[1rem]'
-                >
-                  <div className='flex items-center gap-4'>
-                    <div className='w-[3rem] title-24 text-primary-700 -mb-1'>{item.ranking}등</div>
-                    <div className='relative w-[4.875rem] h-[4.875rem] rounded-sm overflow-hidden'>
-                      <Image
-                        src={item.user.image}
-                        alt='profile image for my ranking'
-                        quality={85}
-                        fill
-                        style={{ objectFit: 'cover' }}
-                      />
-                    </div>
-                    <strong className='title-24 w-[11.25rem] text-primary-700 -mb-1'>{item.user.nickname}</strong>
-                    <p className=' grow title-24 text-white/60 -mb-1'>{item.user.introduction}</p>
-                  </div>
-                  <span className='body-36 ml-auto text-primary-700'>{item.total}점</span>
-                </div>
-              ))}
-              {countRanking?.slice(3, 5)?.map((item) => (
-                <div
-                  key={item.id}
-                  className='top-rank flex items-center bg-[#C5CDF7] w-full h-[4.75rem] px-8 py-[0.375rem] mx-auto rounded-[1rem]'
-                >
-                  <div className='flex items-center gap-4'>
-                    <div className='w-[3rem] title-20 text-primary-700 -mb-1'>{item.ranking}등</div>
-                    <div className='relative w-[3.875rem] h-[3.875rem] rounded-sm overflow-hidden'>
-                      <Image
-                        src={item.user.image}
-                        alt='profile image for my ranking'
-                        quality={85}
-                        fill
-                        style={{ objectFit: 'cover' }}
-                      />
-                    </div>
-                    <strong className='title-20 w-[8.875rem] text-primary-700 -mb-1'>{item.user.nickname}</strong>
-                    <p className=' grow title-20 text-[#647BEE] -mb-1'>{item.user.introduction}</p>
-                  </div>
-                  <span className='body-36 ml-auto text-primary-700'>{item.total}점</span>
-                </div>
-              ))}
-              {countRanking?.slice(5)?.map((item) => (
-                <div
-                  key={item.id}
-                  className='top-rank flex items-center bg-[#C5CDF7] w-full h-[3.25rem] px-8 py-1 mx-auto rounded-[1rem]'
-                >
-                  <div className='flex items-center gap-4'>
-                    <div className='w-[3rem] title-20 text-primary-700 -mb-1'>{item.ranking}등</div>
-                    <div className='relative w-[2.5rem] h-[2.5rem] rounded-sm overflow-hidden'>
-                      <Image
-                        src={item.user.image}
-                        alt='profile image for my ranking'
-                        quality={85}
-                        fill
-                        style={{ objectFit: 'cover' }}
-                      />
-                    </div>
-                    <strong className='title-20 w-[8.875rem] text-primary-700 -mb-1'>{item.user.nickname}</strong>
-                    <p className=' grow title-20 text-[#647BEE] -mb-1'>{item.user.introduction}</p>
-                  </div>
-                  <span className='body-32 ml-auto text-primary-700'>{item.total}점</span>
-                </div>
-              ))}
-            </div>
-          </div>
+      if (myRankingLastWeek !== undefined) {
+        const totalUsers = lastWeekData.length;
+        myRankPercentLastWeek = ((1 - (myRankingLastWeek - 1) / (totalUsers - 1)) * 100).toFixed(2);
+      } else {
+        myRankPercentLastWeek = null;
+      }
+    }
+  }
+  console.log('myRankPercentLastWeek', myRankPercentLastWeek);
+
+  return (
+    <div className='container pt-10 pb-4'>
+      <div
+        className='scrollbar-primary flex flex-col pt-8 pb-[9.375rem] rounded-[3.125rem] bg-primary-50'
+        style={{ height: 'calc(100vh - 136px)' }}
+      >
+        <div className='flex items-center justify-center gap-x-2'>
+          <Image
+            src='/icon_rank.svg'
+            width={45}
+            height={45}
+            quality={85}
+            alt='랭킹순위 옆 아이콘'
+          />
+          <h2 className='title-36 text-primary-400 mt-1 -mb-2'>이번주 전체 랭킹 순위</h2>
         </div>
-
-        <div className='rank-my-info'>
-          <div className='rank-my-info-card'>
-            <div className='relative w-[6.875rem] h-[6.875rem] border border-primary-400 rounded-[1.25rem] overflow-hidden'>
-              <Image
-                src={userProfile?.image ?? ''}
-                alt='profile image for my ranking'
-                quality={85}
-                fill
-                style={{ objectFit: 'cover' }}
-              />
-            </div>
-            <div className='flex grow gap-[2.75rem] h-full pl-[2.125rem]'>
-              <div className='flex items-center grow gap-[2.75rem]'>
-                <div className='flex flex-col self-stretch w-full'>
-                  <div className='flex items-center justify-center body-16 mb-4 text-primary-400'>
-                    {userProfile?.nickname}
-                  </div>
-                  <div className='flex items-center justify-center h-full caption-14 bg-primary-50 rounded-[0.875rem]'>
-                    {userProfile ? userProfile?.introduction : '한줄 소개가 없습니다.'}
-                  </div>
-                </div>
-                <div className='flex flex-col justify-between gap-4 w-full max-w-[12.813rem] title-20'>
-                  <div className='flex items-center justify-between'>
-                    <div className='text-primary-700'>나의 랭킹</div>
-                    <div className='text-primary-400'>{userTable?.[0]?.ranking}등</div>
-                  </div>
-                  <div className='flex items-center justify-between'>
-                    <div className='text-primary-700'>지난주 순위</div>
-                    <div className='text-primary-600'>{myLastRank ? myLastRank?.ranking : ''}등</div>
-                  </div>
-                </div>
-                <div className='flex flex-col justify-between self-stretch w-full max-w-[15.25rem] title-16'>
-                  <div className='flex items-center justify-between'>
-                    <div className='text-primary-700'>나야, 발음왕</div>
-                    <div className='text-primary-600'>{userTable?.[0]?.speaking}점</div>
-                  </div>
-                  <div className='flex items-center justify-between'>
-                    <div className='text-primary-700'>빈칸 한 입</div>
-                    <div className='text-primary-600'>{userTable?.[0]?.writing}점</div>
-                  </div>
-                  <div className='flex items-center justify-between'>
-                    <div className='text-primary-700'>틀린 말 탐정단</div>
-                    <div className='text-primary-600'>{userTable?.[0]?.checking}점</div>
-                  </div>
-                  <div className='flex items-center justify-between'>
-                    <div className='text-primary-700'>총합 점수</div>
-                    <div className='text-primary-600'>{userTable?.[0]?.total}점</div>
-                  </div>
-                </div>
-              </div>
-              <Link
-                href={'/mypage'}
-                className='flex justify-center items-center self-center text-center w-[5.875rem] h-[5.25rem] body-16 bg-primary-400 text-white rounded-[0.875rem]'
+        <div className='h-full mx-4 mt-7 overflow-y-scroll'>
+          <div className='grid gap-5 pl-[4.75rem] pr-[3.813rem] pb-5'>
+            {countRankingThisWeek?.slice(0, 3)?.map((item) => (
+              <div
+                key={item.id}
+                className='top-rank flex items-center bg-[#98A7F1] w-full h-[6.25rem] px-8 py-2 mx-auto rounded-[1rem]'
               >
-                내정보 <br />
-                보러가기
-              </Link>
-            </div>
+                <div className='flex items-center gap-4'>
+                  <div className='w-[3rem] title-24 text-primary-700 -mb-1'>{item.ranking}등</div>
+                  <div className='relative w-[4.875rem] h-[4.875rem] rounded-sm overflow-hidden'>
+                    <Image
+                      src={item.user.image}
+                      alt='profile image for my ranking'
+                      quality={85}
+                      fill
+                      style={{ objectFit: 'cover' }}
+                    />
+                  </div>
+                  <strong className='title-24 w-[11.25rem] text-primary-700 -mb-1'>{item.user.nickname}</strong>
+                  <p className=' grow title-24 text-white/60 -mb-1'>{item.user.introduction}</p>
+                </div>
+                <span className='body-36 ml-auto text-primary-700'>{item.total}점</span>
+              </div>
+            ))}
+            {countRankingThisWeek?.slice(3, 5)?.map((item) => (
+              <div
+                key={item.id}
+                className='top-rank flex items-center bg-[#C5CDF7] w-full h-[4.75rem] px-8 py-[0.375rem] mx-auto rounded-[1rem]'
+              >
+                <div className='flex items-center gap-4'>
+                  <div className='w-[3rem] title-20 text-primary-700 -mb-1'>{item.ranking}등</div>
+                  <div className='relative w-[3.875rem] h-[3.875rem] rounded-sm overflow-hidden'>
+                    <Image
+                      src={item.user.image}
+                      alt='profile image for my ranking'
+                      quality={85}
+                      fill
+                      style={{ objectFit: 'cover' }}
+                    />
+                  </div>
+                  <strong className='title-20 w-[8.875rem] text-primary-700 -mb-1'>{item.user.nickname}</strong>
+                  <p className=' grow title-20 text-[#647BEE] -mb-1'>{item.user.introduction}</p>
+                </div>
+                <span className='body-36 ml-auto text-primary-700'>{item.total}점</span>
+              </div>
+            ))}
+            {countRankingThisWeek?.slice(5)?.map((item) => (
+              <div
+                key={item.id}
+                className='top-rank flex items-center bg-[#C5CDF7] w-full h-[3.25rem] px-8 py-1 mx-auto rounded-[1rem]'
+              >
+                <div className='flex items-center gap-4'>
+                  <div className='w-[3rem] title-20 text-primary-700 -mb-1'>{item.ranking}등</div>
+                  <div className='relative w-[2.5rem] h-[2.5rem] rounded-sm overflow-hidden'>
+                    <Image
+                      src={item.user.image}
+                      alt='profile image for my ranking'
+                      quality={85}
+                      fill
+                      style={{ objectFit: 'cover' }}
+                    />
+                  </div>
+                  <strong className='title-20 w-[8.875rem] text-primary-700 -mb-1'>{item.user.nickname}</strong>
+                  <p className=' grow title-20 text-[#647BEE] -mb-1'>{item.user.introduction}</p>
+                </div>
+                <span className='body-32 ml-auto text-primary-700'>{item.total}점</span>
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    );
-  }
+      <div className='container fixed bottom-4 left-1/2 -translate-x-1/2'>
+        <div className='h-[9.375rem] flex items-center px-6 py-[1.125rem] bg-primary-100 rounded-[1.25rem]'>
+          <div className='relative w-[6.875rem] h-[6.875rem] rounded-[1.25rem] overflow-hidden'>
+            <Image
+              src={userProfile?.image ?? ''}
+              alt='profile image for my ranking'
+              quality={85}
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+          <div className='flex grow gap-[2.75rem] h-full pl-[2.125rem]'>
+            <div className='flex items-center grow gap-[2.75rem]'>
+              <div className='flex flex-col self-stretch w-full'>
+                <div className='flex items-center justify-center body-16 mb-4 text-primary-400'>
+                  {userProfile?.nickname}
+                </div>
+                <div className='flex items-center justify-center h-full caption-14 bg-primary-50 rounded-[0.875rem]'>
+                  {userProfile ? userProfile?.introduction : '한줄 소개가 없습니다.'}
+                </div>
+              </div>
+              <div className='flex flex-col justify-between gap-4 w-full max-w-[12.813rem] title-20'>
+                <div className='flex items-center justify-between'>
+                  <div className='text-primary-700'>나의 랭킹</div>
+                  <div className='text-primary-400'>{myRankingThisWeek?.[0]?.ranking}등</div>
+                </div>
+                <div className='flex items-center justify-between'>
+                  <div className='text-primary-700'>지난주 순위</div>
+                  <div className='text-primary-600'>{myRankingLastWeek ? myRankingLastWeek : ''}등</div>
+                </div>
+              </div>
+              <div className='w-[200px]'>
+                <PercentGraph
+                  thisWeek={myRankPercentThisWeek}
+                  lastWeek={myRankPercentLastWeek}
+                />
+              </div>
+            </div>
+            <Link
+              href={'/mypage'}
+              className='flex justify-center items-center self-center text-center w-[5.875rem] h-[5.25rem] body-16 bg-primary-400 text-white rounded-[0.875rem]'
+            >
+              내정보 <br />
+              보러가기
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RankingPage;
