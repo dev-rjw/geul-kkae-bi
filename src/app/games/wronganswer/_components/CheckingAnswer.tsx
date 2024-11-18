6b1a93ac0c7e8a6fdc6a4f818ed36b3e1e2b33d3
hanamstarfield <qnfdmlto@naver.com>
2024-11-18 17:05:07 +0900

    update: checking 오답 css 수정

--- src/app/games/wronganswer/_components/CheckingAnswer.tsx
+++ src/app/games/wronganswer/_components/CheckingAnswer.tsx
@@ -69,11 +69,11 @@ const CheckingAnswer = () => {
           <button
             onClick={handlePrevious}
             disabled={currentPage === 0}
-            className={`p-3 rounded-full ${
-              currentPage === 0 ? 'bg-gray-300 cursor-not-allowed' : 'bg-[#8E24AA] text-white hover:bg-[#6a1b9a]'
+            className={`w-[3.5rem] h-[3.5rem] rounded-full ${
+              currentPage === 0 ? 'bg-[#A07BE5] text-[#BFA5ED] cursor-not-allowed' : 'bg-[#A07BE5] text-white'
             }`}
           >
-            ←
+            &lt;
           </button>
 
           {/* 카드 영역 */}
@@ -81,12 +81,12 @@ const CheckingAnswer = () => {
             {paginatedAnswers?.map((answer, index) => (
               <div
                 key={index}
-                className='relative card rounded-lg bg-[#fcfbfe] p-5 flex flex-col border h-[509px]'
+                className='relative card rounded-2xl bg-[#fcfbfe] p-5 flex flex-col border h-[509px] w-[442px]'
               >
                 <div className='flex justify-between items-center'></div>
 
                 {/* 문제 및 선택지 */}
-                <div className='mt-3 flex flex-col gap-4'>
+                <div className='flex flex-col gap-4'>
                   {/* 문제 헤더 */}
                   <div className='flex justify-between items-center gap-2'>
                     <span className='bg-[#E6D4F2] text-[#8150dd] font-semibold px-[0.875rem] py-1 rounded-lg text-lg'>
@@ -116,15 +116,15 @@ const CheckingAnswer = () => {
                   </div>
 
                   {/* 문제 텍스트 */}
-                  <p className='text-[#4A148C] text-lg font-semibold'>{answer.question}</p>
+                  <p className='text-[#504f4e] text-2xl font-semibold'>{answer.question}</p>
 
                   {/* 선택지 */}
-                  <div className='flex flex-col gap-2'>
+                  <div className='flex flex-col gap-[0.375rem]'>
                     {Array.isArray(answer.correct) &&
                       answer.correct.map((option: string, idx: number) => (
                         <div
                           key={idx}
-                          className={`flex items-center gap-3 px-3 py-2 rounded-md text-[#504f4e] ${
+                          className={`flex items-center gap-2 px-5 py-2 rounded-md text-[#504f4e] ${
                             answer.user_answer === option
                               ? 'bg-[#fcdede] '
                               : option === answer.answer
@@ -137,34 +137,43 @@ const CheckingAnswer = () => {
                             {idx + 1}
                           </span>
                           {/* 옵션 텍스트 */}
-                          <span className='text-base font-medium'>{option}</span>
+                          <span className='text-2xl font-medium flex items-center justify-between gap-2'>
+                            {option}
+                            {answer.user_answer !== option && option === answer.answer && (
+                              <span className='text-[#4F21A6] font-bold text-base'>O</span>
+                            )}
+                            {answer.user_answer === option && option !== answer.answer && (
+                              <span className='text-[#EF5252] font-bold text-base'>X</span>
+                            )}
+                          </span>
                         </div>
                       ))}
                   </div>
                 </div>
 
                 {/* 정답 해설 */}
-                <div className='absolute bottom-0 left-0 w-full bg-[#ddd0f6] text-[#4A148C] rounded-b-lg px-4 py-3'>
+                <div className='absolute bottom-0 left-0 w-full h-[115px] bg-[#ddd0f6] text-[#4A148C] rounded-b-2xl px-5 py-[1.125rem] '>
                   <div className='flex items-center mb-2 gap-2'>
-                    <span className='bg-[#a07be5] text-[#fcfbfe] px-3 py-1 rounded-lg text-sm font-semibold'>
+                    <span className='bg-[#a07be5] text-[#fcfbfe] px-[0.875rem] py-[0.125rem] rounded-lg text-lg font-semibold'>
                       정답 해설
                     </span>
                   </div>
-                  <div className='flex items-center gap-3'>
+                  <div className='flex items-center gap-[0.313rem]'>
                     {/* 정답 인덱스 */}
-                    <div className='flex items-center gap-1'>
+                    <div className='flex items-center gap-2'>
                       <span className='w-6 h-6 flex items-center justify-center bg-[#a07be5] text-[#ffffff] font-bold rounded-full'>
                         {Array.isArray(answer.correct)
                           ? answer.correct.findIndex((opt: string) => opt === answer.answer) + 1
                           : '-'}
                       </span>
                       {/* 정답 */}
-                      <span className={`text-[#504f4e] text-2xl `}>{answer.answer}</span>
+                      <span className={`text-[#504f4e] text-2xl line-through`}>{answer.answer}</span>
                     </div>
 
                     {/* 화살표 및 해설 */}
-                    <span className='text-[#4A148C] text-2xl font-semibold'>
-                      → <span className='bg-[#b39ddb] px-2 rounded'>{answer.meaning}</span>
+                    <span className='text-[#a07be5] text-2xl font-semibold'>→</span>
+                    <span className='text-[#4f21a6] text-2xl font-semibold underline decoration-[#b39ddb] decoration-'>
+                      {answer.meaning}
                     </span>
                   </div>
                 </div>
@@ -176,10 +185,10 @@ const CheckingAnswer = () => {
           <button
             onClick={handleNext}
             disabled={(currentPage + 1) * itemsPerPage >= checkingWrongAnswers.length}
-            className={`p-3 rounded-full ${
+            className={`w-[3.5rem] h-[3.5rem] rounded-full ${
               (currentPage + 1) * itemsPerPage >= checkingWrongAnswers.length
-                ? 'bg-gray-300 cursor-not-allowed'
-                : 'bg-[#8E24AA] text-white hover:bg-[#6a1b9a]'
+                ? 'bg-[#A07BE5] text-[#BFA5ED] cursor-not-allowed'
+                : 'bg-[#A07BE5] text-white'
             }`}
           >
             &gt;
@@ -194,7 +203,7 @@ const CheckingAnswer = () => {
             className={`w-[21.875rem] h-[3.25rem] font-semibold rounded-lg ${
               selectedQuestions.length === 0
                 ? 'bg-[#D1C4E9] text-[#B39DDB] cursor-not-allowed'
-                : 'bg-[#8E24AA] text-white'
+                : 'bg-[#8150DD] text-white'
             }`}
           >
             {selectedQuestions.length === 0 ? '지우기' : `${selectedQuestions.length}개 지우기`}
