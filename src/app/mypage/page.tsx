a36e47758739e9d52466cc93d6c761937b1bcc69
ayoung <jo.ayoung.1210@gmail.com>
2024-11-20 06:52:17 +0900

    style: 로딩 스타일 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,8 @@
-import { Suspense } from 'react';
 import { redirect } from 'next/navigation';
 import MypageProfile from './_components/MypageProfile';
 import MypageCharacter from './_components/MypageCharacter';
 import MypageMyRank from './_components/MypageMyRank';
 import MypageMyScore from './_components/MypageMyScore';
-import { Loader2 } from 'lucide-react';
 import { fetchCurrentUser } from '@/utils/auth/server-action';
 
 const MyPage = async () => {
@@ -17,23 +15,14 @@ const MyPage = async () => {
   return (
     <div className='container py-10 max-md:pt-0'>
       <div className='flex flex-wrap gap-4 max-lg:flex-col max-lg:flex-nowrap'>
-        <Suspense fallback={<Loader2 className='mr-2 h-4 w-4 animate-spin' />}>
-          <MypageProfile />
-        </Suspense>
+        <MypageProfile />
 
         <div className='flex flex-col gap-4 w-[calc(100%-22.875rem)] max-lg:w-full'>
-          <Suspense fallback={<Loader2 className='mr-2 h-4 w-4 animate-spin' />}>
-            <MypageCharacter />
-          </Suspense>
+          <MypageCharacter />
 
           <div className='flex gap-4'>
-            <Suspense fallback={<Loader2 className='mr-2 h-4 w-4 animate-spin' />}>
-              <MypageMyRank />
-            </Suspense>
-
-            <Suspense fallback={<Loader2 className='mr-2 h-4 w-4 animate-spin' />}>
-              <MypageMyScore />
-            </Suspense>
+            <MypageMyRank />
+            <MypageMyScore />
           </div>
         </div>
       </div>
